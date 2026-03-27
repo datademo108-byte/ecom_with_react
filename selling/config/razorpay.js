3b21f84404fa157418dc53d10c9d3700b66b221e
vivek Agrahari <datademo108@gmail.com>
2026-03-27 12:16:36 +0530

    Delete selling directory

--- selling/config/razorpay.js
+++
@@ -1,8 +0,0 @@
-const Razorpay = require("razorpay");
-
-const razorpay = new Razorpay({
-    key_id: "rzp_test_qwUWbYhsbam1oJ",
-    key_secret: "NKuKFl5Q2PcW5poRLqXNaqOE"
-});
-
-module.exports = razorpay;
